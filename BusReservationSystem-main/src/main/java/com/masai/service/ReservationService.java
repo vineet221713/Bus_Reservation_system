39e1b6fa30405e6ff65e29eadb02759c1a0e929d
Vineet Kumar Singh <100182758+vineet221713@users.noreply.github.com>
2024-02-22 12:41:33 +0530

    Add files via upload

---
+++ BusReservationSystem-main/src/main/java/com/masai/service/ReservationService.java
@@ -0,0 +1,16 @@
+package com.masai.service;
+
+import java.util.List;
+
+import com.masai.exceptions.ReservationException;
+import com.masai.model.Reservation;
+
+public interface ReservationService {
+	public Reservation addReservation(Reservation reservation,int userId,String key) throws ReservationException;
+	public Reservation updateReservation(Reservation reservation, int userId,String key) throws ReservationException;
+	public Reservation deleteReservation(int reservationId, int userId,String key) throws ReservationException;
+	public Reservation viewReservation(int reservationId) throws ReservationException;
+	public List<Reservation> viewAllReservation() throws ReservationException;
+//	public List<Reservation> getAllReservation(LocalDate date) throws ReservationException;
+	
+}
